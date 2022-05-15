266fe6808ce9f1c8f0d693bdcb621ed200cb37a3
Rasmus Aasmäe <s2031389@ed.ac.uk>
2022-05-15 16:24:59 +0100

    add comments

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,11 @@
 const movie = require("../commands/movie");
+
+// called when interaction event happens
 module.exports = (client, interaction) => {
+  // no nothing if not menu selection
   if (!interaction.isSelectMenu()) return;
 
+  // if movie was selected calls function to respond
   if (interaction.customId === "movieAddMenu") {
     const id = interaction.values[0];
     movie.addMovie(interaction, id);
